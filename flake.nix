f534ef63c500e9056eab894214525c1e1c02049a
Ole Krüger <ole@vprsm.de>
2022-01-04 22:19:16 +0000

    Remove broken flake packages

--- flake.nix
+++ flake.nix
@@ -4,17 +4,9 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs;
     flake-utils.url = github:numtide/flake-utils;
-    simpoole = {
-      url = github:vapourismo/simpoole;
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    semigroupoids-src = {
-      url = github:ekmett/semigroupoids;
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, simpoole, semigroupoids-src }: {
+  outputs = { self, nixpkgs, flake-utils }: {
     overlay = final: prev: {
       haskell = prev.haskell // {
         packageOverrides =
@@ -42,22 +34,8 @@
       };
     };
   } // flake-utils.lib.eachDefaultSystem (system:
-    with (import nixpkgs {
-      inherit system;
-      overlays = [ simpoole.overlay self.overlay ];
-    });
-
+    with (import nixpkgs { inherit system; });
     {
-      packages = {
-        psql = haskellPackages.psql;
-      };
-
-      defaultPackage = self.packages.${system}.psql;
-
-      checks = {
-        psql = self.packages.${system}.psql.overrideAttrs (_: { doCheck = true; });
-      };
-
       devShell = mkShell {
         buildInputs = [
           postgresql
